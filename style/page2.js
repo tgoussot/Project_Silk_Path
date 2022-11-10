9665f513a0eb0ae826eec947a88dea4bba577859
Adrial <yonnis.peyrous@gmail.com>
2022-11-10 09:36:00 +0100

    filtre color

---
+++ style/page2.js
@@ -0,0 +1,49 @@
+var check01  =  document.getElementById("c01");
+function check1() {
+    check01.classList.toggle('visible');
+  }
+
+var check02  =  document.getElementById("c02");
+function check2() {
+    check02.classList.toggle('visible');
+  }
+
+var check03  =  document.getElementById("c03");
+function check3() {
+    check03.classList.toggle('visible');
+  }
+
+var check04  =  document.getElementById("c04");
+function check4() {
+    check04.classList.toggle('visible');
+  }
+
+var check05  =  document.getElementById("c05");
+function check5() {
+    check05.classList.toggle('visible');
+  }
+
+var check06  =  document.getElementById("c06");
+function check6() {
+    check06.classList.toggle('visible');
+  }
+
+var check07  =  document.getElementById("c07");
+function check7() {
+    check07.classList.toggle('visible');
+  }
+
+var check08  =  document.getElementById("c08");
+function check8() {
+    check08.classList.toggle('visible');
+  }
+
+var check09  =  document.getElementById("c09");
+function check9() {
+    check09.classList.toggle('visible');
+  }
+
+var check010  =  document.getElementById("c010");
+function check10() {
+    check010.classList.toggle('visible');
+  }
